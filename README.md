f95c11d2157aef2d5c6ff5a5bbeb6a3cb4025268
Jonathan Buckland <funcoder@gmail.com>
2022-08-31 20:50:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meteor / Blaze Project Template
+I created this template for myself so I can start up a Meteor project and develop in Gitpod.
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./imports/ui/**/*.{html,js,jsx,ts,tsx}",
+    "./client/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
